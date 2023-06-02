730593c06e804de3d1e02e336ea62e3718a34de0
Jonatan-Oliveira <jonatan.jopbi@gmail.com>
2023-06-02 14:55:22 -0300

    atualizando a main

--- main.tf
+++ main.tf
@@ -1,4 +1,6 @@
-resource = "google_storage_bucket" "gcs_bucket" {
-    name = "bucket-eng-pipeline-gcp-9114"
-    location = var.region
+resource "google_storage_bucket" "gcs_bucket" {
+  name     = "bucket-eng-pipeline-gcp-9114"
+  location = var.region
+
+  # Additional configuration for the bucket...
 }
